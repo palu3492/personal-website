ccdacc174c3c67c3b65c7973199ae053bde240fe
Alex Palumbo <palu3492@stthomas.edu>
2019-12-21 14:11:37 -0600

    Update

--- app.js
+++ app.js
@@ -22,7 +22,37 @@ function setup(){
                 {text: 'text1', id:1},
                 {text: 'text2', id:2},
                 {text: 'text3', id:3}
-            ]
+            ],
+            projects: {
+                'Websites': [
+                    {
+                        name: 'Flashcards',
+                        content: ''
+                    },
+                    {
+                        name: 'Stage Designer',
+                        content: ''
+                    },
+                    {
+                        name: 'Video Editor',
+                        content: ''
+                    },
+                ],
+                'Game Development': [
+                    {
+                        name: 'Flashcards',
+                        content: ''
+                    },
+                    {
+                        name: 'Stage Designer',
+                        content: ''
+                    },
+                    {
+                        name: 'Video Editor',
+                        content: ''
+                    },
+                ]
+            }
         },
         methods: {
             hideTodos(){
